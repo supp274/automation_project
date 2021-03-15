93ab64fde95cc0a4f6ff61fb6f72ff7aec1b2776
supp274 <54321028+supp274@users.noreply.github.com>
2021-03-15 20:18:36 +0530

    Update automation.sh
    Adding tar file to s3 bucket

--- automation.sh
+++ automation.sh
@@ -2,3 +2,8 @@ sudo apt update
 apt install apache2
 sudo /etc/init.d/apache2 restart
 service apache2 status
+
+aws s3 \
+cp /tmp/supriya-httpd-logs-15032021-081048.tar
+s3://supriya-httpd-logs-15032021-081048.tar
+
